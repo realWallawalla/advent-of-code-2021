47f7fce2624ded2c21af0fd02987de572281f7c5
Timon Sarakinis <timon.sarakinis@gmail.com>
2021-12-01 22:52:38 +0100

    day1 with python. remove some comment

--- day01/main.py
+++ day01/main.py
@@ -25,8 +25,3 @@ if part == 'part2':
     print(getSolutionPart2(file_input))
 else:
     print(getSolutionPart1(file_input))
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    getSolutionPart2(file_input)
