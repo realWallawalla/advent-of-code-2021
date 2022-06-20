f062b931ae6e3245722a3c2086f52117ffa9b81c
Timon Sarakinis <timon.sarakinis@gmail.com>
2022-06-20 21:47:57 +0200

    day 11 solution part1 kotlin

--- day11/src/main/kotlin/Day11.kt
+++ day11/src/main/kotlin/Day11.kt
@@ -2,9 +2,10 @@ import java.io.File
 
 fun main() {
     val input = File("input.txt").readLines()
-        .flatMap { it.split(",") }
-        .map { it.toInt() }
-        .toList()
+        .mapIndexed { y, row ->
+        row.mapIndexed  { x, energy -> Octopus(energy.digitToInt(), x, y) }
+            .toTypedArray() }
+        .toTypedArray()
 
     val answer = when (System.getenv("part")) {
         "part2" -> solutionPart2(input)
@@ -13,10 +14,102 @@ fun main() {
     println(answer)
 }
 
-private fun solutionPart1(grid: List<Int>): Int {
-    return 1
+private fun solutionPart1(grid: Array<Array<Octopus>>): Int {
+    val yMax = grid.size
+    val xMax = grid[0].size
+    var flasherCount = 0
+
+    for (i in 0 until 100) {
+        val hasFlashed = mutableSetOf<Octopus>()
+        for (y in 0 until yMax) {
+            for (x in 0 until xMax) {
+                val octopus = grid[y][x]
+                if (!hasFlashed.contains(octopus)) {
+                    octopus.energyLevel += 1
+                }
+                flasherCount += incNeighborsAndCount(octopus, grid, hasFlashed)
+            }
+        }
+    }
+    return flasherCount
 }
 
-private fun solutionPart2(input: List<Int>): Int {
+fun incNeighborsAndCount(octupus: Octopus, grid: Array<Array<Octopus>>, hasFlashed: MutableSet<Octopus>): Int {
+    if (octupus.energyLevel > 9) {
+        val neighbors = octupus.getNeighbors(grid)
+        octupus.energyLevel = 0
+        hasFlashed.add(octupus)
+        //count flashers
+        return 1 + neighbors
+            .filter { !hasFlashed.contains(it) }
+            .map { it.incEnergyLevel() }
+            .map { incNeighborsAndCount(it, grid, hasFlashed)}
+            .sum()
+    }
+    return 0 //no flash, return 0
+}
+
+private fun solutionPart2(grid: Array<Array<Octopus>>): Int {
     return 2
+}
+
+data class Octopus(var energyLevel: Int, val x: Int, val y: Int) {
+
+    fun incEnergyLevel(): Octopus {
+        this.energyLevel += 1
+        return this
+    }
+    fun getNeighbors(grid: Array<Array<Octopus>>): List<Octopus> {
+        val yMax = grid.size - 1
+        val xMax = grid[0].size - 1
+
+        var neighbors = cornerNeighbors(grid, yMax, xMax)
+        if (neighbors.isEmpty()) {
+            neighbors = borderNeighbors(grid, yMax, xMax)
+        }
+        if (neighbors.isEmpty()) {
+            neighbors = normalNeighbors(grid)
+        }
+
+        return neighbors
+    }
+
+    private fun cornerNeighbors(grid: Array<Array<Octopus>>, yMax: Int, xMax: Int): List<Octopus> {
+        if (this == grid[0][0]) {
+            return mutableListOf(grid[y][x+1], grid[y+1][x], grid[y+1][x+1])
+        } else if (this == grid[0][xMax]) {
+            return mutableListOf(grid[y][x-1], grid[y+1][x], grid[y+1][x-1])
+        } else if (this == grid[yMax][0]) {
+            return mutableListOf(grid[y][x+1], grid[y-1][x], grid[y-1][x+1])
+        } else if (this == grid[yMax][xMax]) {
+            return mutableListOf(grid[y][x-1], grid[y-1][x], grid[y-1][x-1])
+        } else {
+            return emptyList()
+        }
+    }
+
+    private fun borderNeighbors(grid: Array<Array<Octopus>>, yMax: Int, xMax: Int): List<Octopus> {
+        if (y == 0) {
+            return mutableListOf(grid[y+1][x], grid[y][x+1], grid[y][x-1], grid[y+1][x-1], grid[y+1][x+1])
+        } else if (y == yMax) {
+            return mutableListOf(grid[y-1][x], grid[y][x+1], grid[y][x-1], grid[y-1][x-1], grid[y-1][x+1])
+        } else if (x == 0) {
+            return mutableListOf(grid[y+1][x], grid[y-1][x], grid[y][x+1], grid[y-1][x+1], grid[y+1][x+1])
+        } else if (x == xMax) {
+            return mutableListOf(grid[y+1][x], grid[y-1][x], grid[y][x-1], grid[y+1][x-1], grid[y-1][x-1])
+        } else {
+            return emptyList()
+        }
+    }
+
+    private fun normalNeighbors(grid: Array<Array<Octopus>>): List<Octopus> {
+        return mutableListOf(grid[y][x-1],
+            grid[y][x+1],
+            grid[y+1][x],
+            grid[y-1][x],
+            grid[y-1][x-1],
+            grid[y+1][x-1],
+            grid[y-1][x+1],
+            grid[y+1][x+1])
+    }
 }
